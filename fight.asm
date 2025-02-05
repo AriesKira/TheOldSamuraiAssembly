8b509fb70c256d52ac1633dd666b21413c9b8f57
yanislammari <yanis.lammari937@gmail.com>
2025-02-05 18:35:04 +0100

    add player mooves without animation

--- fight.asm
+++ fight.asm
@@ -3,100 +3,108 @@ pile    ends
 
 donnees segment public
 include GFX.inc
-; extrn image:byte
 
 extrn run1:byte
 
-x dw 0  ; Déclaration de la variable x
-y dw 0  ; Déclaration de la variable y
+x dw 10      ; Position X initiale
+y dw 100     ; Position Y initiale
+old_x dw 10  ; Ancienne position X
+old_y dw 100 ; Ancienne position Y
+
+icon_width dw 32  ; Largeur de l'icône (ajuste selon ton image)
+icon_height dw 32 ; Hauteur de l'icône (ajuste selon ton image)
+
 donnees ends
 
 code    segment public
 
 assume cs:code, ds:donnees, es:code, ss:pile
 
-myprog:         ; debut de la zone instructions
-main: 
-
-
-    call Video13h
+init:         
+    call Video13h  ; Active le mode vidéo 13h
     mov AX, donnees
     mov DS, AX
-    ; Affiche l'image de map 1 
-    mov BX, offset run1
-    mov hX, 10
-    mov hY, 10
-    call drawIcon
-    mov tempo, 30
-    mov DX, 100  ; Position X initiale
-    mov x, DX
-    mov DX, 100  ; Position Y initiale
-    mov y, DX
 
-
-main_loop:
-    call PeekKey
-    call sleep
-    cmp userinput, 0
-    je main_loop
-    
-    ; Efface l'image à l'ancienne position
-    mov ax, x
-    mov Rx, ax
-    mov ax, y
-    mov Ry, ax
-    mov Rw, 32  ; Largeur de l'image (à ajuster selon la taille de votre image)
-    mov Rh, 32  ; Hauteur de l'image (à ajuster selon la taille de votre image)
-    mov col, 15  ; Couleur de fond (blanc)
+    ; Dessiner le fond initial
+    mov Rx, 0
+    mov Ry, 0
+    mov Rw, 320
+    mov Rh, 200
+    mov col, 39
     call fillRect
 
-    ; Met à jour les coordonnées en fonction des touches du clavier
+    call draw_image  ; Dessiner l'image initiale
+
+main:
+    mov userinput, 0
+    call PeekKey
+    cmp userinput, 27 ; ESC
+    jne continue
+    jmp fin           ; Utiliser JMP pour éviter l'erreur de saut
+
+continue:
     cmp userinput, 72
-    je moove_up
+    je move_up
     cmp userinput, 80
-    je moove_down
+    je move_down
     cmp userinput, 75
-    je moove_left
+    je move_left
     cmp userinput, 77
-    je moove_right
-    cmp userinput, 27
-    je fin
+    je move_right
 
-    jmp draw_image
+    jmp main
 
-moove_up:
+move_up:
     sub y, 10
-    jmp draw_image
+    jmp update_image
 
-moove_down:
+move_down:
     add y, 10
-    jmp draw_image
+    jmp update_image
 
-moove_left:
+move_left:
     sub x, 10
-    jmp draw_image
+    jmp update_image
 
-moove_right:
+move_right:
     add x, 10
-    jmp draw_image
+    jmp update_image
+
+update_image:
+    ; Effacer l'ancienne image en remplissant la zone avec la couleur de fond
+    mov AX, old_x
+    mov Rx, AX       ; Position X de l'ancien sprite
+    mov AX, old_y
+    mov Ry, AX       ; Position Y de l'ancien sprite
+    mov AX, icon_width
+    mov Rw, AX       ; Largeur de l'icône
+    mov AX, icon_height
+    mov Rh, AX       ; Hauteur de l'icône
+    mov col, 39      ; Couleur de fond
+    call fillRect    ; Effacer l'ancienne image
+
+    call draw_image  ; Dessiner l'image à la nouvelle position
+    jmp main
 
 draw_image:
-    ; Redessine l'image à la nouvelle position
-    mov ax, x
-    mov Hx, ax
-    mov ax, y
-    mov hY, ax
+    ; Sauvegarder l'ancienne position
+    mov AX, x
+    mov old_x, AX
+    mov hX, AX
+
+    mov AX, y
+    mov old_y, AX
+    mov hY, AX
+
     mov BX, offset run1
     call drawIcon
-
     mov userinput, 0
-    jmp main_loop
+    ret
 
 fin:
-    mov AH, 4Ch  ; 4Ch = fonction exit DOS
-    mov AL, 00h  ; code de sortie 0 (OK)
+    mov AH, 4Ch  ; Quitter le programme
+    mov AL, 00h  
     int 21h
 
-code    ends               ; Fin du segment de code
-
-end myprog                 ; Fin du programme
+code    ends
+end init
